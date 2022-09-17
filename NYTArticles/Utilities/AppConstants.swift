4bb8e303140ee1e5d4fe85e87350c0f9971beb1b
yuhangang <70849672+yuhangang@users.noreply.github.com>
2022-09-17 15:22:04 +0800

    update api key

--- NYTArticles/Utilities/AppConstants.swift
+++ NYTArticles/Utilities/AppConstants.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 public struct AppConstants {
-    static let apiKey: String = "P979U9nqzOFdHCE4ZKumlkpd3W38oz97"
+    static let apiKey: String = "1tqYGGTGklj02mALbym6tJ0OWpUYqhZT"
     
     static let apiEndpoint: URL = URL(string: "https://api.nytimes.com/svc/")!
 }
